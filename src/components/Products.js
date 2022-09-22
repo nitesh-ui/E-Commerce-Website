8a425834aa4b7a7ee98f47ad76c8438818880413
nitesh-ui <112393577+nitesh-ui@users.noreply.github.com>
2022-09-22 08:16:20 -0700

    Add files via upload

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Product from './Product';
 
 const products = [
-    {id: 1, name: 'Shoes', desc: 'Running Shoes', price: 10, img: 'https://rukminim1.flixcart.com/image/495/594/l0pm3680/shoe/e/9/w/9-fx3631-9-adidas-ftwwht-ftwwht-gretwo-original-imagcfspcm7nhh8y.jpeg?q=50'},
+    {id: 1, name: 'Shoes', desc: 'Running Shoes', price: 10, img: 'https://rukminim1.flixcart.com/image/495/594/kfk0e4w0-0/shoe/6/m/m/brd-343-abisto-black-original-imafvzgynd8aybhj.jpeg?q=50'},
     {id: 2, name: 'Macbook', desc: 'Apple Macbook', price: 15, img: 'https://rukminim1.flixcart.com/image/312/312/kp5sya80/screen-guard/tempered-glass/o/v/n/apple-macbook-air-m1-13-3-inch-lightwings-original-imag3gh5xftgbpg3.jpeg?q=70'},
     {id: 3, name: 'Mobile', desc: 'Samsung S7', price: 20, img: 'https://rukminim1.flixcart.com/image/312/312/l4n2oi80/mobile/x/o/a/-original-imagfhu75eupxyft.jpeg?q=70'},
     {id: 4, name: 'Pen', desc: 'Raynold Pen', price: 25, img: 'https://rukminim1.flixcart.com/image/612/612/l2z26q80/pen/4/t/6/-original-image6tfumcvgsrg.jpeg?q=70'}
