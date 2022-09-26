2e9dcffae471fa90530f5716c0cc38012af4a329
nitesh-ui <112393577+nitesh-ui@users.noreply.github.com>
2022-09-25 22:18:04 -0700

    Add files via upload

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,26 +9,32 @@ const Cart = ({cart}) => {
                 <div className='col-md-12 text-center'>
                     <h4>Your Shopping Cart</h4>
                 </div>
-                {cart.line_items && cart.line_items.map((item) => (
-                    <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12 my-3'>
-                        <div className='row d-flex border p-3'>
-                            <div className='col-md-2'>
-                                <img src={item.image.url} className="product-image" alt="..." />
-                            </div>
-                            <div className='col-md-10'>
-                                <div className="cartProduct-details">
-                                    <h5 className="product-name">{item.name}</h5>
-                                    <p className="product-desc">{item.desciption}</p>
-                                    <p className="product-quantity">Quantity : {item.quantity}</p>
-                                    <p className="product-price"><span>{item.price.formatted_with_symbol}</span></p>
+                {cart.total_items = 0 ? <div>There is no product in cart</div> : 
+                    <div>
+                        {cart.line_items && cart.line_items.map((item) => (
+                            <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12 my-3'>
+                                <div className='row d-flex border p-3'>
+                                    <div className='col-md-2'>
+                                        <img src={item.image.url} className="product-image" alt="..." />
+                                    </div>
+                                    <div className='col-md-10'>
+                                        <div className="cartProduct-details">
+                                            <h5 className="product-name">{item.name}</h5>
+                                            <p className="product-desc">{item.desciption}</p>
+                                            <p className="product-quantity">Quantity : {item.quantity}</p>
+                                            <p className="product-price"><span>{item.price.formatted_with_symbol}</span></p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        ))}
                     </div>
-                ))}
-                <div className='cartDetails'>
-                    <p><strong>Subtotal:</strong> {cart.subtotal.formatted_with_symbol}</p>
-                </div>
+                }
+                {cart.total_items = 0 ? '' : 
+                    <div className='cartDetails'>
+                        <p><strong>Subtotal: </strong>{cart.subtotal.formatted_with_symbol}</p>
+                    </div>
+                }
             </div>
         </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,19 +7,22 @@ import "tippy.js/animations/perspective.css";
 import "tippy.js/animations/scale.css";
 import MoreTooltip from './MoreTooltip';
 import LoginTooltip from './LoginTooltip';
+import { Link } from 'react-router-dom';
 
 const Header = ({totalItem}) => {
     return (
         <div className='header'>
             <div className='header_first'>
-                <img src='//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png' alt='Logo' />
-                <div className='header_first1'>
-                    <span style={{fontSize: '11px', color: 'white', fontStyle: 'italic'}}>Explore</span>
-                    <span style={{fontSize: '11px', color: '#f9e107', fontStyle: 'italic'}}>Plus</span>
-                    <span>
-                        <img width="10px" src='//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png' alt='Logo Plus' />
-                    </span>
-                </div>
+                <Link to="/">
+                    <img src='//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png' alt='Logo' />
+                    <div className='header_first1'>
+                        <span style={{fontSize: '11px', color: 'white', fontStyle: 'italic'}}>Explore</span>
+                        <span style={{fontSize: '11px', color: '#f9e107', fontStyle: 'italic'}}>Plus</span>
+                        <span>
+                            <img width="10px" src='//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png' alt='Logo Plus' />
+                        </span>
+                    </div>
+                </Link>
             </div>
             <div className='header_second'>
                 <input placeholder="Search for products..." type="text" />
@@ -37,7 +40,7 @@ const Header = ({totalItem}) => {
                 </Tippy>
             </div>
             <div className='header_fourth'>
-                <p>Become a Seller</p>
+                <Link to="/products"><p>All Products</p></Link>
             </div>
             <div className='header_fifth'>
                 <Tippy
@@ -52,10 +55,12 @@ const Header = ({totalItem}) => {
                 </Tippy>
             </div>
             <div className='header_sixth'>
-                <i className="bi bi-cart-fill position-relative">
-                    <span className="position-absolute top-0 start-100 translate-middle badge text-bg-danger">{totalItem}</span>
-                </i>
-                <p>Cart</p>
+                <Link to="/cart">
+                    <i className="bi bi-cart-fill position-relative">
+                        <span className="position-absolute top-0 start-100 translate-middle badge text-bg-danger">{totalItem}</span>
+                    </i>
+                    <p>Cart</p>
+                </Link>
             </div>
         </div>
     )
